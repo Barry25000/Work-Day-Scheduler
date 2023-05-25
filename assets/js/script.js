c53cc27343b3720021426d8767d9811dceadd2cf
Barry25000 <barryengler@gmail.com>
2023-05-25 16:36:24 -0500

    file update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-
+  
+  gettextFromStorage();
+  var storedText
     // code to display the current date in the header of the page.
   var currentDay = $("#currentDay");
   currentDay.text(dayjs().format("MMM D, YYYY"));
@@ -12,41 +14,42 @@ $(function () {
   var currentHour=dayjs().format('HH');
   console.log(currentHour);
 
-  // TODO: Add a listener for click events on the save button. This code should
+  // listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    
-    $('.saveBtn').on("click", function(e){
-      e.preventDefault();
-      
-      var description = $(".description");
-      saveEventsToStorage();
-      console.log(description);
-           
-    })
-    
-
-
-
-
-
     //Takes an array of events and saves them to local storage.
-          // need a for loop to extract the data from the array, looking for parent element(time block info) snd value (textarea info). I need to apend all the info into 1 element and then save in local storage
+    
+       
+    $('.time-block').on('click', '.saveBtn', function() {
+      var description = $(this).siblings('.description').val();
+      var buttonId = $(this).parent().attr("id");
+      // console.log(description);
+      localStorage.setItem(buttonId, description); 
+      console.log(storedText);
+    });
 
-          
-    function saveEventsToStorage(description) {
-      localStorage.setItem('description', JSON.stringify(description));
-     
+
+    // need a for loop to extract the data from the array, looking for parent element(time block info) snd value (textarea info). I need to apend all the info into 1 element and then save in local storage
+
+
+    function gettextFromStorage () {
+      storedText = localStorage.getItem("buttonId");
+      if (storedText) {
+        storedText = JSON.parse(storedText);
+      } else {
+        storedText = []
+      }
+
+      return storedText
+
     }
 
-    
-  
-  
-  
-    // code to apply the past, present, or future class to each time
+
+
+     // code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
